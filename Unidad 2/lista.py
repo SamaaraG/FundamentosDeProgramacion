258f2ecaf733023d67f68af655d624f842adb596
SamaaraG <samaraagallegos@gmail.com>
2025-09-18 07:55:36 -0600

    Create lista.py

---
+++ Unidad 2/lista.py
@@ -0,0 +1,13 @@
+#Lista de compañeros
+lista=["Juan","Ivan","Adrian","Jair"]
+print (lista)
+#Imprimir lista
+print(lista[2])
+lista.append("Alexa")
+print(lista)
+lista.insert(1,"Javier")
+print(lista)
+#Imprimir lista pop
+lista.pop()
+lista.remove("Juan")
+lista.pop(1)
